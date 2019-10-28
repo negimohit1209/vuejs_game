cbb24263fc440f8ecc57b2e40f7538a149c01b93
negimohit1209 <mohitsngh1997@gmail.com>
2019-10-28 16:47:15 +0530

    attack implemented

--- app.js
+++ app.js
@@ -10,6 +10,46 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function () {
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            if (this.checkWin()) {
+                return;
+            }
+            damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.checkWin();
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+
+        },
+        calculateDamage: function (minDamage, maxDamage) {
+            return Math.floor(Math.random() * maxDamage - minDamage) + minDamage;
+        },
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won, start a new game.')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('monster won, start a new game.')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     },
 })
